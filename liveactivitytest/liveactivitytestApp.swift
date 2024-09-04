bc33cff641767e107947373f3206f7469ce47509
Will Bishop <17292672+WillBishop@users.noreply.github.com>
2024-09-04 20:15:17 +1000

    Initial Commit

---
+++ liveactivitytest/liveactivitytestApp.swift
@@ -0,0 +1,17 @@
+//
+//  liveactivitytestApp.swift
+//  liveactivitytest
+//
+//  Created by Will Bishop on 4/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct liveactivitytestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
